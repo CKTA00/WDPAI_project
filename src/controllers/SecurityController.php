082b7dd4cd7945c3185e4960427bbfc9b4520926
CKTA00 <przekozuch@gmail.com>
2021-12-30 11:46:06 +0100

    Changed views file extension to .php
    Small change to SecurityController

---
+++ src/controllers/SecurityController.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "AppController.php";
+
+class SecurityController extends AppController
+{
+
+}
--- src/models/User.php
+++ src/models/User.php
@@ -8,12 +8,62 @@ class User
     private $surname;
     private $password;
 
-    public function __construct($email, $login, $name, $surname, $password)
+    public function __construct(string $email, string $login, string $name, string $surname, string $password)
     {
         $this->email = $email;
         $this->login = $login;
         $this->name = $name;
         $this->surname = $surname;
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
         $this->password = $password;
     }
 }
