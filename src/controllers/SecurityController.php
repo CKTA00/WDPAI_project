cb76de7128cb247814e393ae882c3d86a432e56c
CKTA00 <przekozuch@gmail.com>
2022-01-20 15:48:21 +0100

    Tiny refactor of login

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -6,32 +6,38 @@ require_once __DIR__."/../repository/UserRepository.php";
 
 class SecurityController extends AppController
 {
+    private $userRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->userRepository = new UserRepository();
+    }
+
     public function login_user()
     {
-        $userRepository = new UserRepository();
-
         if(!$this->isPost())
         {
             return $this->render('login');
         }
         $login = $_POST["login"];
-        $password = $_POST["password"];
+        $password = sha256($_POST["password"]);
 
-        $user = $userRepository->getUser($login);
+        $user = $this->userRepository->getUser($login);
         if(!$user)
         {
-            return $this->render("login",["messages"=>["User does not exists."]]);
+            return $this->render("login",["messages"=>["User does not exist."]]);
         }
 
         if($login !== $user->getEmail() && $login !== $user->getLogin())
         {
-            return $this->render("login",["messages"=>["User with this email or login does not exists."]]);
+            return $this->render("login",["messages"=>["User with this email or login does not exist."]]);
         }
         if($password !== $user->getPassword())
         {
             return $this->render("login",["messages"=>["Wrong password."]]);
         }
-        //return $this->render("announcements");
+
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/announcements");
     }
