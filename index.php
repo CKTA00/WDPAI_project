1f0d35b6d7f67b0bfea9ca6ef60ca6587ed9fb94
CKTA00 <przekozuch@gmail.com>
2022-02-03 15:51:09 +0100

    Enabled routing to chats

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ Router::get('get_announcement_JSON','DashboardController');
 Router::get('follow','FollowersController');
 Router::get('unfollow','FollowersController');
 //chats endpoints:
-//Router::get('chats','DashboardController');
+Router::get('chats','DashboardController');
 //options endpoints:
 Router::get('options','OptionsController');
 Router::post('change_profile_image','OptionsController');
